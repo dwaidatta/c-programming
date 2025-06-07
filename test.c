1a704556ee1ffe5e37e98c9f6df5eadb6345dc7e
Dwaipayan Datta <dwaidatta@gmail.com>
2025-06-08 03:23:48 +0530

    testing

--- test.c
+++ test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // printf("Hello, World!\n");
+    int m, n;
+    int a = scanf("%d %d", &m, &n);
+    printf("%d", a);
+
     return 0;
 }
